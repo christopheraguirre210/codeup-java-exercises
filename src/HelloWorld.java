9cfb4b46207cf5beb7b11ca38c6d6d8202c20575
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-20 11:08:13 -0600

    first class exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Hello World!";
+        System.out.println(name);
+        System.out.print(name);
+    }
+}
