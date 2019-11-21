ff45b23f22ec68f0586c7af6e2bbe419e597afab
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-21 14:24:25 -0600

    FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -16,8 +16,20 @@ public class ControlFlowExercises {
 //            System.out.println("i is " + i);
 //        }
 
-        for(long i = 2; i < 1000000; i*=i) {
-            System.out.println(i);
+//        for(long i = 2; i < 1000000; i*=i) {
+//            System.out.println(i);
+//        }
+
+        for(int i = 1; i < 100; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else{
+                System.out.println(i);
+            }
         }
     }
 }
