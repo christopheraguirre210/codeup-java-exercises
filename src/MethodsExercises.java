47a176e03d408590b0c77eac7ea6b10f30f5ad48
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-26 09:36:07 -0600

    methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,12 +4,12 @@ import  java.lang.Math;
 public class MethodsExercises {
 
     public static void main(String[] args) {
-        System.out.println("add(1,2 = " + addition(1,2));
-        System.out.println(addition(1,2) == 3.0);
-
-        System.out.print("Enter a number between 1 and 10: ");
-        int userInput = getInteger(1, 10);
-        System.out.println("userInput = " + userInput);
+//        System.out.println("add(1,2 = " + addition(1,2));
+//        System.out.println(addition(1,2) == 3.0);
+//
+//        System.out.print("Enter a number between 1 and 10: ");
+//        int userInput = getInteger(1, 10);
+//        System.out.println("userInput = " + userInput);
 //        addition(2,2);
 //        subtraction(5,2);
 //        multiplication(5,2);
@@ -18,7 +18,7 @@ public class MethodsExercises {
 //        getInteger(1,10);
 //        factorial(5);
 //        diceRoll();
-//        adventureGame();
+        adventureGame();
     }
 
 //    public static void addition(int num1, int num2){
@@ -41,105 +41,116 @@ public class MethodsExercises {
 //        System.out.println(num1 % num2);
 //    }
 
-    public static double addition(double num1, double num2){
-        return num1 + num2;
-    }
-    public static double subtraction(double num1, double num2){
-        return num1 - num2;
-    }
-    public static double multiplication(double num1, double num2){
-        return num1 * num2;
-    }
-    public static double division(double num1, double num2){
-        return num1 / num2;
-    }
-    public static double modulus(double num1, double num2){
-        return num1 % num2;
-    }
-
-//    public static void getInteger(int min, int max){
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please Enter a number");
-//        int inputNum = scanner.nextInt();
-//        if(inputNum >= min && inputNum <= max){
-//            System.out.println("That works for me");
-//        }else{
-//            System.out.println("Your number is not in range");
-//            getInteger(min,max);
-//        }
+//    public static double addition(double num1, double num2){
+//        return num1 + num2;
+//    }
+//    public static double subtraction(double num1, double num2){
+//        return num1 - num2;
+//    }
+//    public static double multiplication(double num1, double num2){
+//        return num1 * num2;
+//    }
+//    public static double division(double num1, double num2){
+//        return num1 / num2;
+//    }
+//    public static double modulus(double num1, double num2){
+//        return num1 % num2;
 //    }
 
-    public static int getInteger(int min, int max){
-
-        return 0;
+    public static void getInteger(int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter a number");
+        int inputNum = scanner.nextInt();
+        if(inputNum >= min && inputNum <= max){
+            System.out.println("That works for me");
+        }else{
+            System.out.println("Your number is not in range");
+             getInteger(min,max);
+        }
     }
 
-    public static void factorial(int num){
+//    public static int getInteger(int min, int max){
+//
+//        return 0;
+//    }
 
-        ////// BOOLEAN USED FOR USER CONTINUE LATER ///////
-
-        boolean userContinue = true;
-
-        do {
-            for(int i = 1; i <= num; i++){
-                long factorSolution = 1;
-                String message = i + "! = ";
-
-                for(int x = 1; x <= i; x++){
-                    if(i == x){
-                        factorSolution *= x;
-                        message += x;
-                        continue;
-                    }
-                    factorSolution *= x;
-                    message += x + " x ";
-                }
-                message += " = " + factorSolution;
-                System.out.println(message);
-            }
-
-            System.out.println("Do you want to continue? (y/n)");
-            Scanner userCon = new Scanner(System.in);
-            String userCont = userCon.next();
-
-            if(userCont.equals("y")){
-                System.out.println("Choose a new number to factorial: ");
-
-                int newUserInt = userCon.nextInt();
-                num = newUserInt;
-                userContinue = true;
-            }else{
-
-                userContinue = false;
-            }
-
-            //////// USER CONTINUE BEGINS AT FALSE, WILL ONLY RUN AGAIN IF USER ENTERS "y" //////
-
-        }while(userContinue);
-    }
+//    public static void factorial(int num){
+//
+//        //// BOOLEAN USED FOR USER CONTINUE LATER ///////
+//
+//        boolean userContinue = true;
+//
+//        do {
+//            for(int i = 1; i <= num; i++){
+//                long factorSolution = 1;
+//                String message = i + "! = ";
+//
+//                for(int x = 1; x <= i; x++){
+//                    if(i == x){
+//                        factorSolution *= x;
+//                        message += x;
+//                        continue;
+//                    }
+//                    factorSolution *= x;
+//                    message += x + " x ";
+//                }
+//                message += " = " + factorSolution;
+//                System.out.println(message);
+//            }
+//
+//            System.out.println("Do you want to continue? (y/n)");
+//            Scanner userCon = new Scanner(System.in);
+//            String userCont = userCon.next();
+//
+//            if(userCont.equals("y")){
+//                System.out.println("Choose a new number to factorial: ");
+//
+//                int newUserInt = userCon.nextInt();
+//                num = newUserInt;
+//                userContinue = true;
+//            }else{
+//
+//                userContinue = false;
+//            }
+//
+//            //////// USER CONTINUE BEGINS AT FALSE, WILL ONLY RUN AGAIN IF USER ENTERS "y" //////
+//
+//        }while(userContinue);
+//    }
+//
+//        public static long calculateFactorial(){
+//            int number = getInteger(1,10);
+////            4
+//            long output = 1;
+//            for (int counter = 1; counter <= number; counter++){
+//
+//                output *= counter;
+//            }
+//            return output;
+//        }
 
     public static int randomNumber(int number){
         return (int) (Math.random() * (number)) + 1;
     }
 
-    public static void diceRoll(){
-       Scanner sc = new Scanner(System.in);
-
-       System.out.println("How many sides do these dice have?");
-       int sides = sc.nextInt();
-
-       if(sides > 6){
-           System.out.println("We use normal dice here, kid\nTry again:");
-           diceRoll();
-       }else if(sides < 1){
-           System.out.println("Take this serious!\nTry again:");
-           diceRoll();
-       }else{
-           int dice = randomNumber(sides);
-       }
-
-
-    }
+//    public static int diceRoll(){
+//       Scanner sc = new Scanner(System.in);
+//
+//       System.out.println("How many sides do these dice have?");
+//       int sides = sc.nextInt();
+//
+//       if(sides > 6){
+//           System.out.println("We use normal dice here, kid\nTry again:");
+//           diceRoll();
+//       }else if(sides < 1){
+//           System.out.println("Take this serious!\nTry again:");
+//           diceRoll();
+//       }else{
+//           int dice = randomNumber(sides);
+//       }
+//
+//
+//    }
 
     public static void adventureGame(){
         int mageHealth = (int) (Math.random() * 20) + 50;
@@ -183,6 +194,8 @@ public class MethodsExercises {
             System.out.println("Welcome " + userName);
             System.out.println("Choose your class: Press 1 for mage or 2 for warrior!");
             fighterClassNum = sc.nextInt();
+        }else{
+            System.out.println("I knew you didn't have the guts");
         }
         if (fighterClassNum == 1) {
             fighterClass = "Mage";
