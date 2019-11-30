c5217fc6702053a81ed91d15677b387c4fff0f17
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-30 10:19:53 -0600

    finished the circle app part of the exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,15 +1,5 @@
 package shapes;
 
-import util.Input;
-
-class CircleApp {
-    public static void main(String[] args) {
-        Input input = new Input();
-        Circle circle = new Circle(input.getDouble());
-        System.out.println("circle.getArea() = " + circle.getArea());
-        System.out.println("circle.getCircumference() = " + circle.getCircumference());
-    }
-}
 
 public class Circle {
 
