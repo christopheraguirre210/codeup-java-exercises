1b60795cdbbedd94c2a8f5837eb58b66a28e48a0
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-30 10:32:40 -0600

    attempted to work on the circle bonus

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,13 +2,25 @@ package shapes;
 
 import util.Input;
 
+import java.util.Scanner;
+
 public class CircleApp {
 
         public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
             Input input = new Input();
+
             Circle circle = new Circle(input.getDouble());
             System.out.println("circle.getArea() = " + circle.getArea());
             System.out.println("circle.getCircumference() = " + circle.getCircumference());
+            System.out.println("Would you like to make another circle? " + input.yesNo());
+            sc.nextLine();
+
+            if(input.yesNo()){
+                System.out.println(input.getDouble());
+            }
 
         }
 }
