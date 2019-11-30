718476b82ac8fe813ae52dcbbbfa4aa9e965619d
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-30 12:13:34 -0600

    Arrays exercise 1.

---
+++ src/ArraysExercises.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        String[] Person = {"Chris", "Michelle", "Riley"};
+
+        for(int i = 0; i < Person.length; i++){
+            System.out.println(Person[i]);
+        }
+
+    }
+
+    public static void addPerson(){
+        
+    }
+}
