4a0ea2be84c1ba0cbd3303779ffad66815c04a12
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-09 09:28:48 -0600

    note about what the method is doing

--- src/util/PracticeQuestions.java
+++ src/util/PracticeQuestions.java
@@ -2,6 +2,8 @@ package util;
 
 public class PracticeQuestions {
 
+
+//    adds every number before the passed number and returns the total
     public static int SimpleAdding(int num){
         int bucket = 0;
         for(int i = 1; i <= num; i++){
