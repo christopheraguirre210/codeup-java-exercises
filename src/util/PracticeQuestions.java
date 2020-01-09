0ae2753c5177bc49503b8a207dae4966e970c1b3
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-09 09:26:22 -0600

    morning java warmup

---
+++ src/util/PracticeQuestions.java
@@ -0,0 +1,16 @@
+package util;
+
+public class PracticeQuestions {
+
+    public static int SimpleAdding(int num){
+        int bucket = 0;
+        for(int i = 1; i <= num; i++){
+            bucket += i;
+        }
+        return bucket;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
