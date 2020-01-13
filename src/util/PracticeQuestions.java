6977c13e1d2e5ebcd5f5952976f5d702864e505f
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-13 09:36:01 -0600

    method that takes in two array lists and merges them and sorts them on the return

--- src/util/PracticeQuestions.java
+++ src/util/PracticeQuestions.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class PracticeQuestions {
 
 
@@ -10,6 +14,19 @@ public class PracticeQuestions {
             bucket += i;
         }
         return bucket;
+    }
+
+    public static ArrayList<Integer> CombineList(ArrayList<Integer> ListOne, ArrayList<Integer> ListTwo){
+        ArrayList<Integer> ListThree = new ArrayList<>();
+        for(int i = 0; i < ListOne.size(); i++){
+            ListThree.add(ListOne.get(i));
+            Collections.sort(ListThree);
+        }
+        for(int j = 0; j < ListTwo.size(); j++){
+            ListThree.add(ListTwo.get(j));
+            Collections.sort(ListThree);
+        }
+        return ListThree;
     }
 
     public static void main(String[] args) {
